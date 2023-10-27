0bdcfe61c8cf7a03397896c340a49af9f16f70ad
Yannick Chabbert <yannick.chabbert.it@gmail.com>
2023-10-27 17:47:56 +0200

    fix broken tests

--- folioblog/core/tests/selenium/test_not_found.py
+++ folioblog/core/tests/selenium/test_not_found.py
@@ -10,7 +10,6 @@ class NotFoundPageLiveTestCase(FolioBlogSeleniumServerTestCase):
 
         HomePageFactory(parent=self.site.root_page)
         self.image_bg = ImageFactory(title="404-bg")
-        self.image_body = ImageFactory(title="404-body")
 
         url = f"{self.live_server_url}/givemea404please"
         self.webpage = NotFoundWebPage(self.selenium)
@@ -21,12 +20,3 @@ class NotFoundPageLiveTestCase(FolioBlogSeleniumServerTestCase):
         rendition = self.image_bg.get_rendition(spec)
         rendition_url = f"{self.live_server_url}{rendition.url}"
         self.assertEqual(rendition_url, self.webpage.get_masterhead_image())
-
-    def test_image(self):
-        data = self.webpage.get_image_with_caption()
-        spec = "width-940" if self.is_mobile else "width-700"
-        rendition = self.image_body.get_rendition(spec)
-        rendition_url = f"{self.live_server_url}{rendition.url}"
-        self.assertEqual(rendition_url, data["img_src"])
-        self.assertIn(rendition.alt, data["caption"])
-        self.assertIn(str(self.image_body.photographer), data["caption"])
--- folioblog/core/tests/units/test_not_found.py
+++ folioblog/core/tests/units/test_not_found.py
@@ -19,12 +19,9 @@ class NotFoundPageTestCase(TestCase):
     def test_404_image(self):
         image_bg = ImageFactory(title="404-bg")
         rendition_bg = image_bg.get_rendition("fill-1905x560")
-        image_body = ImageFactory(title="404-body")
-        rendition_body = image_body.get_rendition("width-700")
 
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 404)
         html = unescape(response.content.decode())
 
         self.assertIn(rendition_bg.url, html)
-        self.assertIn(rendition_body.url, html)
