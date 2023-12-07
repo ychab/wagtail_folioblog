225419448e6a15b55ea749f5fc6140aaa29a8b7e
Yannick Chabbert <yannick.chabbert.it@gmail.com>
2023-12-07 09:36:57 +0100

    black formatting for settings too

--- folioblog/settings/dev.py
+++ folioblog/settings/dev.py
@@ -4,26 +4,26 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS += [
-    'wagtail.contrib.styleguide',
-    'django_extensions',
-    'debug_toolbar',
+    "wagtail.contrib.styleguide",
+    "django_extensions",
+    "debug_toolbar",
 ]
 
-INTERNAL_IPS = ('127.0.0.1',)
-MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+INTERNAL_IPS = ("127.0.0.1",)
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-for logger in LOGGING['loggers'].values():
-    logger['handlers'] = ['console']
-    logger['level'] = 'DEBUG'
+for logger in LOGGING["loggers"].values():
+    logger["handlers"] = ["console"]
+    logger["level"] = "DEBUG"
 
 AUTH_PASSWORD_VALIDATORS = []
 
-WAGTAILADMIN_BASE_URL = 'http://127.0.0.1:8000'
+WAGTAILADMIN_BASE_URL = "http://127.0.0.1:8000"
 WAGTAILAPI_BASE_URL = WAGTAILADMIN_BASE_URL
 
 try:
--- folioblog/settings/test.py
+++ folioblog/settings/test.py
@@ -4,20 +4,18 @@ from .base import *
 
 INSTALLED_APPS += [
     # Well... @see https://github.com/wagtail/wagtail/issues/1824
-    'wagtail.contrib.search_promotions',
+    "wagtail.contrib.search_promotions",
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 
 # Boost perf a little
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 
-STATIC_ROOT = os.path.join(gettempdir(), 'folioblog', 'static')
-MEDIA_ROOT = os.path.join(gettempdir(), 'folioblog', 'media')
+STATIC_ROOT = os.path.join(gettempdir(), "folioblog", "static")
+MEDIA_ROOT = os.path.join(gettempdir(), "folioblog", "media")
 
-WAGTAILADMIN_BASE_URL = 'http://127.0.0.1:8000'
+WAGTAILADMIN_BASE_URL = "http://127.0.0.1:8000"
 WAGTAILAPI_BASE_URL = WAGTAILADMIN_BASE_URL
 
 DEBUG_TEST = True
@@ -29,11 +27,11 @@ except ImportError:  # pragma: no cover
     pass
 
 if DEBUG_LOG:  # pragma: no cover
-    for logger in LOGGING['loggers'].values():
-        logger['handlers'] = ['debug_file', 'stream']
-        logger['level'] = 'DEBUG'
+    for logger in LOGGING["loggers"].values():
+        logger["handlers"] = ["debug_file", "stream"]
+        logger["level"] = "DEBUG"
     # Anyway, remove logging for migrations
-    LOGGING['loggers']['django.db.backends.schema'] = {
-        'handlers': ['null'],
-        'propagate': False,
+    LOGGING["loggers"]["django.db.backends.schema"] = {
+        "handlers": ["null"],
+        "propagate": False,
     }
